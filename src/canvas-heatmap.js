fc3946f55f2f7649b97d222d73643884f3155833
ymarcon <yannick.marcon@epfl.ch>
2025-04-16 15:16:13 +0200

    feat: callback on brush selection clear

--- src/canvas-heatmap.js
+++ src/canvas-heatmap.js
@@ -1197,6 +1197,7 @@ const addBrush = (
       select("#brush-indicator").remove();
       // Clear the brush selection
       brushGroup.call(brush.move, null);
+      options.select([]);
     }
   }
 };
